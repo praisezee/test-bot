158d53efdc79155cd1fdeb45bdbf4dcc23e76823
dev_apus <folorunsopraise580@gmail.com>
2024-05-21 22:33:26 +0100

    amost done

--- src/context/MainContext.jsx
+++ src/context/MainContext.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const MainContext = createContext({})
 
 export const MainProvider = ( { children } ) =>
 {
+  const navigate = useNavigate()
   const [user,setUser] = useState({})
   const [ counter, setCounter ] = useState( 1 );
-  const [value,setValue] = useState(0)
+  const [ value, setValue ] = useState( 0 )
+  const [diss,setDiss] =useState(null)
   const [fadingNumber,setFadingNumber] = useState([])
   
 
@@ -34,20 +37,38 @@ export const MainProvider = ( { children } ) =>
   useEffect( () =>
   {
     setUser( { ...user, tap: value } )
+    setDiss(value/1000)
   }, [ value ] )
   
   useEffect( () =>
   {
-    const app = window.Telegram?.WebApp;
+    try {
+      const app = window.Telegram?.WebApp;
     console.log( app );
-    console.log(app.initDataUnsafe)
-    setUser(app.initDataUnsafe)
+      console.log( app.initDataUnsafe );
+      /* app.CloudStorage.getItem( app.initDataUnsafe.user.id, ( data ) =>
+    {
+      if ( data ) {
+        const retrivedData = JSON.parse( data )
+          setUser( retrivedData )
+          setCounter(retrivedData?.counter || 1)
+      } else {
+        console.error( "No data Found" )
+        navigate('/create')
+      }
+
+    }) */
+    setUser( { ...app.initDataUnsafe.user, tap: 0 } )
+    setDiss(value/100)
+    } catch (error) {
+      console.error(error.message)
+    }
   },[])
 
 
 
   return (
-    <MainContext.Provider value={ { counter, fadingNumber, handleClick, user } }>
+    <MainContext.Provider value={ { counter, fadingNumber, handleClick, user,diss } }>
       {children}
     </MainContext.Provider>
   )
